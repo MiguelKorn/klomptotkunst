a3dc9fc0a0226d91cc0c5bf52b974b815386fe8e
Miguel Korn <20448@ma-web.nl>
2017-01-12 09:43:57 +0100

    changed index to show .tpl files

--- index.php
+++ index.php
@@ -1,35 +1,64 @@
 <?php
 
 // Get username, password from database
-include( 'includes/config.php' );
+include 'includes/config.php';
 // Load Smarty library
-require( 'libs/Smarty.class.php' );
+require 'libs/Smarty.class.php';
 // Initialize
-include( 'includes/bootstrap.php' );
-// Make database connection
-include( 'includes/database.php' );
+include 'includes/bootstrap.php';
 
-require('includes/functs.php');
+// include helpers
+require_once 'helpers/Database.php';
+require_once 'helpers/Model.php';
 
-$templateParser->assign('headerText', 'Dit is de header');
+include 'model/Landingspage.php';
+$landingspage = new Landingspage();
 
-$templateParser->display('header.tpl');
+// get action from url
+$action = isset($_GET['action']) ? $_GET['action'] : 'home';
 
-// Assign value of page title to the smarty variable 'title', usually the value comes from a database
-$templateParser->assign( 'title', 'Me First And The Gimme Gimmes' );
+// get action from url
+$lang = isset($_GET['lang']) ? $_GET['lang'] : 'nl';
 
-// Display template: output html
-$templateParser->display( 'head.tpl' );
+// display header, nav, search-bar
+if ($action == 'agenda' || $action == 'profile' || $action == 'search') {
+    //search only header
+    if ($action == 'search') {
+        $templateParser->display('partials/head.tpl');
+    } else {
+        $templateParser->display('partials/head.tpl');
+        $templateParser->display('partials/nav.tpl');
+    }
+} else {
+    $templateParser->display('partials/head.tpl');
+    $templateParser->display('partials/search-bar.tpl');
+    $templateParser->display('partials/nav.tpl');
+}
 
-// Get newsarticles from database
-include( 'model/select_newsarticles.php' );
+switch ($action) {
+    case 'home':
+        $templateParser->display('index.tpl');
+        break;
+    case 'agenda':
+        $templateParser->display('agenda.tpl');
+        break;
+    case 'contact':
+        $templateParser->display('contact.tpl');
+        break;
+    case 'locations':
+        $templateParser->display('locations.tpl');
+        break;
+    case 'profile':
+        $templateParser->display('profile.tpl');
+        break;
+    case 'search':
+        $templateParser->display('search.tpl');
+        break;
+    default:
+        $templateParser->display('404.tpl');
 
-$templateParser->assign('result', $result);
+}
 
-$templateParser->display('newsarticle.tpl');
-//$templateParser->display('about.tpl');
-
-$templateParser->assign('footerText', '&copy; Miguel Korn 2016');
-
-$templateParser->display('footer.tpl');
+// display footer
+include_once 'views/partials/footer.php';
 
